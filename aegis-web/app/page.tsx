422ec2141c42b21424d154e4c7650d7c5d589f2f
belladev250 <lissadev17@gmail.com>
2025-07-14 14:41:20 +0200

    new changes

--- aegis-web/app/page.tsx
+++ aegis-web/app/page.tsx
@@ -102,7 +102,7 @@ export default function Home() {
 
    <div className="flex gap-4 border-2 border-borderColor p-4 rounded-lg cursor-pointer">
     <Image src='/about.png' alt="default" width={80} height={80} className="self-center" />
-    <Link href="/category/research-projects" className='mt-4 inline-block text-maroon'>
+    <Link href="/projects" className='mt-4 inline-block text-maroon'>
       <div className="flex items-center gap-2">
         <p className="text-maroon text-xl">Research Projects</p>
         <IoChevronForward className="text-maroon text-3xl" />
@@ -112,7 +112,7 @@ export default function Home() {
   
    <div className="flex gap-4 border-2 border-borderColor p-4 rounded-lg cursor-pointer">
     <Image src='/about.png' alt="default" width={80} height={80} className="self-center" />
-    <Link href="/category/research-events" className='mt-4 inline-block text-maroon'>
+    <Link href="/revents" className='mt-4 inline-block text-maroon'>
       <div className="flex items-center gap-2">
         <p className="text-maroon text-xl">Aegis Research Events</p>
         <IoChevronForward className="text-maroon text-3xl"/>
